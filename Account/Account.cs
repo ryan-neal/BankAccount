910df1bfff129e8fd45b7d178972caa37f468ce2
ryan-neal <ryan.neal0@gmail.com>
2021-06-25 11:28:28 -0500

    added file structure

---
+++ Account/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Account
+{
+    public class BankAccount
+    {
+        public string CustomerName { get; set; }
+        public int Balance { get; set; }
+        public string Type { get; set; }
+
+        //constructor
+        public BankAccount()
+        {
+        }
+
+        //constructor2
+        public BankAccount(string customerName, int balance)
+        {
+            this.CustomerName = customerName;
+            this.Type = "checkings";
+            this.Balance = balance;
+        }
+
+        public void Debit(int amount)
+        {
+            if (this.Balance - amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("Not enough money in your account");
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("You can't remove negative money");
+            }
+            this.Balance -= amount;
+        }
+
+        public void Credit(int amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("You can't add negative money");
+            }
+            this.Balance += amount;
+        }
+
+        public void ChangeType(string type)
+        {
+            if (type.ToLower().Equals("checkings") | type.ToLower().Equals("savings"))
+            {
+                this.Type = type.ToLower();
+            }
+        }
+    }
+}
---
+++ AccountTests/UnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Account;
+using System;
+
+namespace AccountTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        BankAccount bank = new BankAccount("John Walker", 50);
+
+        [TestMethod]
+        public void BankAccountHasName()
+        { 
+            //Would ask students about accessing internal bank properties here as you write the test
+            Assert.AreEqual(bank.CustomerName, "John Walker");
+        }
+
+        [TestMethod]
+        public void BankAccountHasMoney()
+        {
+            //Same as above
+            Assert.AreEqual(bank.Balance, 50);
+        }
+
+        [TestMethod]
+        public void DebitWorks()
+        {
+            bank.Debit(20);
+            Assert.AreEqual(bank.Balance, 30);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "Allowed user to remove negative money")]
+        public void DebitDoesntAllowNegativeAmounts()
+        {
+            bank.Debit(-20);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "Allowed user to debit more than their balance")]
+        public void DebitDoesntAllowNegativeBalance()
+        {
+            bank.Debit(-60);
+        }
+
+        [TestMethod]
+        public void CreditWorks()
+        {
+            bank.Credit(30);
+            Assert.AreEqual(bank.Balance, 80);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "Allowed user to add negative money")]
+        public void CreditDoesntAllowNegativeAmounts()
+        {
+            bank.Credit(-20);
+        }
+
+        [TestMethod]
+        public void ChangeTypeWorksWithLowerCase()
+        {
+            bank.ChangeType("savings");
+            Assert.AreEqual(bank.Type, "savings");
+        }
+
+        [TestMethod]
+        public void ChangeTypeWorksWithAnyCase()
+        {
+            bank.ChangeType("SaViNgS");
+            Assert.AreEqual(bank.Type, "savings");
+        }
+
+        [TestMethod]
+        public void ChangeTypeOnlyAllowsCheckingsOrSavings()
+        {
+            bank.ChangeType("broccoli");
+            Assert.AreEqual(bank.Type, "checkings");
+        }
+    }
+}
